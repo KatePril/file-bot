c40f7bc007f6d3c16b33161122871bd93fad1c7d
KatePril <ket14pril14@ukr.net>
2023-11-19 21:26:17 +0200

    Add download function REQUIRES FIXING!!!!!

--- handlers/send_handlers.py
+++ handlers/send_handlers.py
@@ -2,10 +2,16 @@ from aiogram import Router, F
 from aiogram.types import Message
 from os import listdir
 from aiogram.enums import ParseMode
-
+from main import bot
 
 router = Router()
 
 @router.message(F.text == "/list_files")
 async def list_files(message: Message):
     await message.answer("\n".join([f'`{file}`' for file in listdir("files/")]), parse_mode=ParseMode.MARKDOWN)
+
+@router.message(F.text == "/download test.txt")
+async def send_file(message: Message):
+    file = message.text.split(" ")[-1]
+    document = open(path=f"files/{file}")
+    await bot.send_document(document=document)
