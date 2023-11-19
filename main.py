fdb0e5c37d4c2c43a4c4f2e377f5b87298f00ba5
KatePril <ket14pril14@ukr.net>
2023-11-19 19:16:09 +0200

    Add download files handlers

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from settings import settings
-from handlers import text_handlers
+from handlers import text_handlers, download_handlers
 
+bot = Bot(token=settings.BOT_TOKEN.get_secret_value())
 
 async def main():
-    bot = Bot(token=settings.BOT_TOKEN.get_secret_value())
     dp = Dispatcher()
 
+    dp.include_router(download_handlers.router)
     dp.include_router(text_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
